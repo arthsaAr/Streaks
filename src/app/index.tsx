1e76c1f77792b7b36cdef8d9d46e851ed78b002c
arthsaAr <kcraashtra431qwe@gmail.com>
2026-09-10 01:43:16 -0600

    project init and tailwind setup

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center bg-[#121212]">
+      <Text className="text-4xl font-bold text-white">
+        12d 4h 32m
+      </Text>
+    </View>
+  );
+}
